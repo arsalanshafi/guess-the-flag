94173b639aa3e62a767e73748e406c3e49ff0885
abuArsalan <arsalanshafi911@gmail.com>
2024-02-28 09:44:20 +0530

    commit

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 const IMAGE = document.querySelector("img");
+const NEXT = document.querySelector(".nxt");
+const OPTIONS = Array.from(document.querySelectorAll(".option"))
+let currect;
+
 let countryNames;
-const countryCodes = [];
+let countryCodes;
 
-function getFlag() {
-    IMAGE.setAttribute("src", "https://flagcdn.com/224x168/ua.png");
+NEXT.addEventListener("click", () => {
+    getRandomNumbers();
+})
+
+window.onload = getRandomNumbers;
+
+OPTIONS.forEach(e => {
+    e.addEventListener("click", e => { 
+        if(e.target.textContent === currect) alert("correc");
+    })
+})
+
+function putFlag(index) {
+    IMAGE.setAttribute("src", `https://flagcdn.com/128x96/${countryCodes[index]}.png`);
 }
 
-async function getCountries(){
+async function getCountries() {
     blob = await fetch("https://flagcdn.com/en/codes.json");
-    countryNames = await blob.json();
-    countryCodes.push(Object.keys(countryNames));
-    console.log()
+    data = await blob.json();
+    countryCodes = Object.keys(data);
+    countryNames = Object.values(data);
 }
 
-function getRandomCountry(){
-    let random = Math.random()*306;
+async function getRandomNumbers() {
+    const random = [];
+    while (random.length < 4) {
+        let num = Math.random() * 306;
+        if (!random.includes(num)) random.push(Math.floor(num));
+    }
+    await getCountries();
+    currect = countryNames[random[0]];
+    putFlag(random[0]);
+    setOptions(random);
 }
 
-getFlag();
-getCountries();
+function setOptions(arr) {
+    OPTIONS.forEach(e => {
+        let num = Math.floor(Math.random() * arr.length)
+        e.textContent = countryNames[arr[num]];
+        arr.splice(num, 1);
+    })
+}
+
+
+
